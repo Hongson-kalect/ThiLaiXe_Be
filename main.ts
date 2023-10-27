08aaee3c7667c6c281c1a9c0ddd1430209c82dfe
hongson <hongson197201@gmail.com>
2023-10-27 10:24:04 +0700

    create main file outside the src

---
+++ main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from 'src/app.module';
+
+// import { parseGGDriverLink } from 'z.parseGGDriverLink';
+
+async function bootstrap() {
+  // parseGGDriverLink(
+  //   'https://drive.google.com/file/d/1nuA147cZdrlVrjjQNts-FhxHt6VwpD3G/view?usp=drive_link',
+  // );
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.FRONTEND_DOMAIN });
+  app.setGlobalPrefix('api');
+  await app.listen(3001);
+}
+bootstrap();
